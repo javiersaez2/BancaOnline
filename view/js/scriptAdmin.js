61606095be47a90d81b33d49288e163eaa4324d5
mrajado <m_rajado@fpzornotza.com>
2022-12-12 12:50:17 +0100

    z<x

--- view/js/scriptAdmin.js
+++ view/js/scriptAdmin.js
@@ -4,10 +4,8 @@ MyApp.controller('miController', function($scope, $http){
     /////cargar los datos de la tabla usuario de la base de datos 
     $http.get('../../controller/controlador_consulta_usuarios.php')
     .then(function(response) {
-<<<<<<< HEAD
-=======
+
         console.log(response.data.list);
->>>>>>> 7fddfbab80c41ffddbbf16be31072925c20131e7
         $scope.usuarios = response.data.list;
     })
     .catch(function(response) {
