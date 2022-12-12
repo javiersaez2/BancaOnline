1777933b92d5457dbcbfcf5e6fa0243b0d0fa4ec
javiersaez2 <asaez22.az@gmail.com>
2022-12-12 12:46:40 +0100

    JS

--- view/js/scriptAdmin.js
+++ view/js/scriptAdmin.js
@@ -1,10 +1,10 @@
 var MyApp = angular.module('MyApp',[]);
 
-app.controller('miController', function($scope, $http){
+MyApp.controller('miController', function($scope, $http){
     /////cargar los datos de la tabla usuario de la base de datos 
-    $http.get('controlador/controlador_consulta_usuarios.php')
+    $http.get('../../controller/controlador_consulta_usuarios.php')
     .then(function(response) {
-        $scope.usuarios = response.list;
+        $scope.usuarios = response.data.list;
     })
     .catch(function(response) {
         console.error('Error occurred:', response.status, response.data)
