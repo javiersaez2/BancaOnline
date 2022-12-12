5f20abd680565dfbfacb56f4181a81b2c3cf5b31
javiersaez2 <asaez22.az@gmail.com>
2022-12-12 11:58:47 +0100

    Modelos

---
+++ model/ClienteModel.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'connect_data.php';
+include_once 'ClienteClass.php';
+
+class ClienteModel extends ClienteClass
+{
+    private $link;
+    //  public $objFamilia;
+
+    public function OpenConnect()
+    {
+        $konDat = new connect_data();
+        try {
+            $this->link = new mysqli($konDat->host, $konDat->userbbdd, $konDat->passbbdd, $konDat->ddbbname);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        $this->link->set_charset("utf8"); // honek behartu egiten du aplikazio eta
+        //                  //databasearen artean UTF -8 erabiltzera datuak trukatzeko
+    }
+
+    public function CloseConnect()
+    {
+        mysqli_close($this->link);
+    }
+
+}
