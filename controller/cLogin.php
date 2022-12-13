15e60f4b04a52263d03a3c60423873aacaa00441
AnderC <a_caballero@fpzornotza.com>
2022-12-13 08:46:49 +0100

    Login controller cambios

--- controller/cLogin.php
+++ controller/cLogin.php
@@ -3,18 +3,10 @@ include_once '../model/C.php';
 
 $data=json_decode(file_get_contents("php://input"),true);
 
-<<<<<<< HEAD
 $user = new ClienteModel();
 $user->nombre=$data["izena"];
 $user->pasahitza=$data["pasahitza"];
 // $check = $user->setUserData();
-=======
-$user = new userModel();
-$user->username=$data["userName"];
-$user->keyword=$data["keyWord"];
-$check = $user->setUserData();
->>>>>>> 792de4a2eb6b1aa10e304100b3bc639cad04fded
-
 $response = array();
 
 if ($check == 0) {
