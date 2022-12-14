36b646102a097c67ba85b339845fdf6626e42da8
AnderC <a_caballero@fpzornotza.com>
2022-12-14 10:38:12 +0100

    Cambios Login

--- view/js/scriptAdmin.js
+++ view/js/scriptAdmin.js
@@ -117,10 +117,10 @@ MyApp.controller('miController', function($scope, $http){
 
     $scope.logout=function(){
         $http({
-            url: "controller/cLogout.php",
+            url: "/controller/cLogout.php",
             method: "POST"
         }).then(function () {
-            window.location.href = "index.html";
+            window.location.href = "/index.html";
         }).catch(function () {
             console.error("Ocurrio un error", response.status, response.data);
         })	
