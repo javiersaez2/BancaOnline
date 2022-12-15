56da5cf9c3b67568d0b5cd37fc2426a30b2d365f
javiersaez2 <asaez22.az@gmail.com>
2022-12-15 14:32:48 +0100

    Stilos

--- view/js/scriptAdmin.js
+++ view/js/scriptAdmin.js
@@ -1,27 +1,27 @@
-var MyApp = angular.module('MyApp',[]);
+var MyApp = angular.module('MyApp', []);
 
-MyApp.controller('miController', function($scope, $http){
+MyApp.controller('miController', function ($scope, $http) {
     $scope.butonLogOut = true;
-    $scope.cuenta=[]
+    $scope.cuenta = []
     /////cargar los datos de la tabla usuario de la base de datos 
     verusuarios()
 
-    
-    function verusuarios(){
-    $http.get('../../controller/controlador_consulta_usuarios.php')
-    .then(function(response) {
 
-        console.log(response.data.list);
-        $scope.usuarios = response.data.list;
-    })
-    .catch(function(response) {
-        console.error('Error occurred:', response.status, response.data)
-    })
+    function verusuarios() {
+        $http.get('../../controller/controlador_consulta_usuarios.php')
+            .then(function (response) {
+
+                console.log(response.data.list);
+                $scope.usuarios = response.data.list;
+            })
+            .catch(function (response) {
+                console.error('Error occurred:', response.status, response.data)
+            })
     }
 
 
 
-    $scope.borrarUsuario= function (miIndex, item){
+    $scope.borrarUsuario = function (miIndex, item) {
         console.log(item.dniCliente)
         datos = item.dniCliente;
         var datosjson = JSON.stringify(datos)
@@ -33,47 +33,48 @@ MyApp.controller('miController', function($scope, $http){
         }).then(function (response) {
             alert("Funciona")
             verusuarios();
-        },function (error){
+        }, function (error) {
             console.error("Ocurrio un error", response.status, response.data)
         })   //
     }
-        //INSERTAR CLIENTE//
+    //INSERTAR CLIENTE//
     $scope.insertarVista = 'false';
     $scope.listaInsertar = [];
 
-    $scope.nuevoUsuario=function(){
-      
-        document.getElementById("demo-modal2").style.visibility = "visible";
-document.getElementById("demo-modal2").style.opacity= 1;
+    $scope.nuevoUsuario = function () {
+
+        document.getElementById("demo-modal1").style.visibility = "visible";
+        document.getElementById("demo-modal1").style.opacity = 1;
     }
 
-    $scope.nuevoCliente=function(){
-       
-        
+    $scope.nuevoCliente = function () {
+
+
         if ($scope.contrsenaIns != $scope.vefIns) {
             alert("Las claves no coinciden");
             return false;
         }
-        
+
         $scope.listaInsertar = {
             dni: $scope.dniIns,
-            nombre: $scope.nombreIns, 
-            contrasena: $scope.contrsenaIns, 
+            nombre: $scope.nombreIns,
+            contrasena: $scope.contrsenaIns,
         };
         var datosInsert = JSON.stringify($scope.listaInsertar);
         console.log(datosInsert)
         ////////FETCH DE INSERTAR/////
-        $http({url: '../../controller/c_insertarClientes.php', 
-                method: 'POST',
-                params: {value: datosInsert}
-        })    
-            .then (function (response) {
-                alert('Datos insertados con exito '+ response.data);
+        $http({
+            url: '../../controller/c_insertarClientes.php',
+            method: 'POST',
+            params: { value: datosInsert }
+        })
+            .then(function (response) {
+                alert('Datos insertados con exito ' + response.data);
                 console.log(response.data.list.secreto);
-                $scope.insertarVista = 'false';        
+                $scope.insertarVista = 'false';
 
             })
-            .catch(function (response){
+            .catch(function (response) {
                 console.log('Error ocurred: ', response.status);
                 console.log('Error ocurred: ', response.data);
 
@@ -81,55 +82,57 @@ document.getElementById("demo-modal2").style.opacity= 1;
     }
 
 
-////////// - Mostrar cuenta corriente - /////////
-    $scope.MostrarCuentas=function(miIndex, item){
-        $scope.cuenta=[];
-console.log(miIndex)
-console.log("--")
-console.log(item.objCuenta.iban)
-for(i=0;i<item.objCuenta.length;i++){
-$scope.cuenta.push({iban:item.objCuenta[i].iban,dniCliente:item.objCuenta[i].dniCliente,titular:item.objCuenta[i].titular,saldo:item.objCuenta[i].saldo});
-console.log($scope.cuenta)}
-document.getElementById("demo-modal").style.visibility = "visible";
-document.getElementById("demo-modal").style.opacity= 1;
+    ////////// - Mostrar cuenta corriente - /////////
+    $scope.MostrarCuentas = function (miIndex, item) {
+        $scope.cuenta = [];
+        console.log(miIndex)
+        console.log("--")
+        console.log(item.objCuenta.iban)
+        for (i = 0; i < item.objCuenta.length; i++) {
+            $scope.cuenta.push({ iban: item.objCuenta[i].iban, dniCliente: item.objCuenta[i].dniCliente, titular: item.objCuenta[i].titular, saldo: item.objCuenta[i].saldo });
+            console.log($scope.cuenta)
+        }
+        document.getElementById("demo-modal0").style.visibility = "visible";
+        document.getElementById("demo-modal0").style.opacity = 1;
     }
 
-    $scope.cerrarCuentas=function(){
-        document.getElementById("demo-modal").style.visibility = "hidden";
-document.getElementById("demo-modal").style.opacity= 0;
-    
+    $scope.cerrarCuentas = function (numero) {
+        console.log(numero)
+        document.getElementById("demo-modal"+numero+"").style.visibility = "hidden";
+        document.getElementById("demo-modal"+numero+"").style.opacity = 0;
+
     }
     ////////Update//////////
 
-    $scope.modificarUsuario=function(miIndex, item){
+    $scope.modificarUsuario = function (miIndex, item) {
         document.getElementById("dniModificar").disabled = true;
         $scope.modificarVista = 'true';
         $scope.insertarVista = 'false';
         console.log(item)
-        $scope.dniModificar=item.dniCliente;
-        $scope.nombreModificar=item.nombre;
-       $scope.contrasenaModificar=item.pasahitza;
-        $scope.vefModificar=item.pasahitza;
+        $scope.dniModificar = item.dniCliente;
+        $scope.nombreModificar = item.nombre;
+        $scope.contrasenaModificar = item.pasahitza;
+        $scope.vefModificar = item.pasahitza;
 
     }
-    $scope.guardarCliente=function(){
-        dniCliente=$scope.dniModificar;
-        nombre=$scope.nombreModificar;
-        pasahitza= $scope.contrasenaModificar;
-        if($scope.contrasenaModificar==$scope.vefModificar){
+    $scope.guardarCliente = function () {
+        dniCliente = $scope.dniModificar;
+        nombre = $scope.nombreModificar;
+        pasahitza = $scope.contrasenaModificar;
+        if ($scope.contrasenaModificar == $scope.vefModificar) {
             $http({
                 url: "../../controller/controller_update.php",
                 method: "POST",
-                params: { 'dniCliente': dniCliente,'nombre':nombre,'pasahitza':pasahitza }
-            }).then(function(response) {
+                params: { 'dniCliente': dniCliente, 'nombre': nombre, 'pasahitza': pasahitza }
+            }).then(function (response) {
                 alert("Los datos han guardado")
                 $scope.modificarVista = 'false';
                 verusuarios()
             })
-            .catch(function(response) {
-                console.error('Error occurred:', response.status, response.data)
-            })
-        }else{
+                .catch(function (response) {
+                    console.error('Error occurred:', response.status, response.data)
+                })
+        } else {
             alert("Contraseñas no son iguales")
         }
     }
@@ -141,14 +144,14 @@ document.getElementById("demo-modal").style.opacity= 0;
     ///////////////////////////////
     ///////////////////////////////
 
-    $scope.loggedVerify=function() {
+    $scope.loggedVerify = function () {
         $http({
             url: "/controller/cLoggedVerify.php",
             method: "POST"
         }).then(function (response) {
-            if (response.data.error != "logged"){
-                if (window.location.pathname != "/index.html"){
-                    window.location.href="/index.html"
+            if (response.data.error != "logged") {
+                if (window.location.pathname != "/index.html") {
+                    window.location.href = "/index.html"
                 }
 
                 $scope.cuentaUsuario = false;
@@ -158,27 +161,27 @@ document.getElementById("demo-modal").style.opacity= 0;
                 $scope.butonLogOut = true;
                 $scope.butonLogin = false;
 
-                if (response.data.tipo == 1){
+                if (response.data.tipo == 1) {
                     $scope.botonAdmin = true;
                     $scope.cuentaUsuario = false;
                     $scope.users = true;
                 } else {
-                    alert("User: " + response.data.izena + " | Sin acceso, tipo: " + response.data.tipo); 
+                    alert("User: " + response.data.izena + " | Sin acceso, tipo: " + response.data.tipo);
                     $scope.cuentaUsuario = true;
                     $scope.botonAdmin = false;
                     $scope.users = false;
 
-                    if (window.location.pathname != "/index.html"){
+                    if (window.location.pathname != "/index.html") {
                         window.location.href = "/index.html";
                     }
                 }
             }
         }).catch(function (response) {
             console.error("Ocurrio un error", response.status, response.data);
-        })	   
-    }	
+        })
+    }
 
-    $scope.logout=function(){
+    $scope.logout = function () {
         $http({
             url: "/controller/cLogout.php",
             method: "POST"
@@ -187,7 +190,7 @@ document.getElementById("demo-modal").style.opacity= 0;
             $scope.butonLogOut = false;
         }).catch(function () {
             console.error("Ocurrio un error", response.status, response.data);
-        })	
+        })
     }
 })
 
