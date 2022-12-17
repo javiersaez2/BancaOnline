3851a52ba666dc4f28ee7828ace43196f6d1aaef
javiersaez2 <asaez22.az@gmail.com>
2022-12-17 11:12:05 +0100

    Slider de simuladores

--- view/js/index.js
+++ view/js/index.js
@@ -40,3 +40,18 @@ miApp.controller('miControlador', function($scope, $http){
         })	
     }
 });
+
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
