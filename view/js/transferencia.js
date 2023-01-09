667fd2032b27e5d2c26aeb2a05ec20c7c4165ef0
mrajado <m_rajado@fpzornotza.com>
2023-01-09 13:09:11 +0100

    Cuentas a las que transferir

--- view/js/transferencia.js
+++ view/js/transferencia.js
@@ -5,8 +5,9 @@ MyApp.controller('miController', function ($scope, $http) {
     //$scope.butonLogOut = true;
     //$scope.cuenta = []
     /////cargar los datos de la tabla usuario de la base de datos 
-    vercuentas()
+    vercuentas();
 
+    //cuentasNoPersonales
     function vercuentas() {
         $http.post('/controller/c_mostrar_cuentasPersonales.php')
             .then(function (response) {
@@ -17,6 +18,20 @@ MyApp.controller('miController', function ($scope, $http) {
             .catch(function (response) {
                 console.error('Error occurred:', response.status, response.data)
             })
+    }
+
+
+    vercuentasNoPersonales();
+    function vercuentasNoPersonales(){
+        $http.post('/controller/c_mostrar_cuentasTransferir.php')
+        .then(function (response) {
+            console.log(response.data.list);
+            $scope.cuentasNoPersonales = response.data.list;
+            
+        })
+        .catch(function (response) {
+            console.error('Error occurred:', response.status, response.data)
+        })
     }
 
 
