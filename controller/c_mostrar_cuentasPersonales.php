010d094915f4ca2da9ab016b3d09517bf6a627a7
mrajado <m_rajado@fpzornotza.com>
2023-01-09 12:41:12 +0100

    probarvariablestransferencia

--- controller/c_mostrar_cuentasPersonales.php
+++ controller/c_mostrar_cuentasPersonales.php
@@ -4,9 +4,9 @@ include_once '../model/cuenta_corrienteModel.php';
 $response=array();
     
 $cuenta=new cuenta_corrienteModel();
-$cuenta->dni=$_SESSION["dni"];
+$cuenta->setDniCliente($_SESSION["dni"]);
     
-$response['list']= $cuenta->setListCuentasPersonales();
+$response['list']= $cuenta->setListCuenta();
 $response['error']='no error';
     
 echo json_encode($response);
