1dcac469c8fe3a7696f2c6b572bdeb6b610010cf
Fatima-Azza <f_azza@fpzornotza.com>
2023-01-10 11:41:58 +0100

    ingresar

--- controller/c_ingresar.php
+++ controller/c_ingresar.php
@@ -1,9 +1,9 @@
 <?php
 include_once '../model/movimientoModel.php';
 include_once '../model/cuenta_movimientoModel.php';
+include_once '../model/cuenta_corrienteModel.php';
 
 $data = json_decode(file_get_contents("php://input"),true);
-
 $response = array();
 
 $movimiento = new movimientoModel();
@@ -18,6 +18,10 @@ $cuentaMovimiento->setIban($data["iban"]);
 $cuentaMovimiento->setIdMovimiento($id);
 $cuentaMovimiento->setCantidad($data["cantidad"]);
 $response["cuentaMov"] = $cuentaMovimiento->insert();
+
+$cuenta = new cuenta_corrienteModel();
+$cuenta -> setSaldo($data["cantidad"]);
+$cuenta -> setIban($data["iban"]);
 
 
 echo json_encode($response);
