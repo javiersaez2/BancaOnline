4ac92989c2e8a6584d8ad5437df8bd948afbb488
javiersaez2 <asaez22.az@gmail.com>
2023-01-11 13:16:56 +0100

    Comprobar contaseña

--- view/js/cuenta.js
+++ view/js/cuenta.js
@@ -1,6 +1,6 @@
 
 var miApp = angular.module('miApp', []);
-
+//Nav
 miApp.controller('miControlador', function($scope, $http){
     $scope.passMostrar = true;
     $scope.iniciarSesionSection = true; 
@@ -45,6 +45,8 @@ miApp.controller('miControlador', function($scope, $http){
     }
 });
 
+
+//Datos de usuario
 miApp.controller('datoscliente', function ($scope, $http) {
 
     $scope.passMostrar = true;
@@ -59,6 +61,152 @@ miApp.controller('datoscliente', function ($scope, $http) {
             $scope.infocuenta=response.data.list
             $scope.infocorriente=response.data.list.objCuenta
             console.log($scope.infocorriente)
+            console.log($scope.infocuenta);
         })
     }
+
+    $scope.modificarUsuario = function (numero) {
+        modalvisible(numero);
+        $scope.modificarVista = 'true';
+        $scope.insertarVista = 'false';
+        // $scope.dniModificar = item.dniCliente;
+        // $scope.nombreModificar = item.nombre;
+        // $scope.contrasenaModificar = item.pasahitza;
+        // $scope.vefModificar = item.pasahitza;
+    }
+
+    $scope.guardarPassword = function () {
+        pasahitza="34561"
+        pruebas=JSON.stringify({'pasahitza': pasahitza})
+console.log(pruebas)
+        $http({
+            url: '/controller/c_comprobarpassword.php',
+            method: "POST",
+            data: JSON.stringify({'pasahitza': pasahitza})
+        }).then(function (response) {
+            console.log(response);
+
+    })
+        /*if ($scope.passanti!=$scope.infocuenta.pasahitza){
+            console.log($scope.errores)
+            $scope.errores="Contaseña incorrecta"
+        }
+        
+        else if ($scope.passModificar!=$scope.veriModificar){
+            $scope.errores="Los campos de contraseña no coinciden "
+         }
+         else{
+ /* $http({
+            url: '../../controller/c_insertarCuenta.php',
+            method: "POST",
+            data: JSON.stringify({'dniCliente': dni, 'nombre': nombreCliente})
+        }).then(function (response) {
+            alert(response.data.error)
+            verusuarios();
+        }).catch(function (response) {
+            console.error('Error occurred:', response.status, response.data)
+        }) 
+         }
+
+      
+        }*/
+    }
+    
+    $scope.cerrarCuentas = function (numero) {
+        $scope.dniIns = "";
+        $scope.nombreIns = "";
+        $scope.contrsenaIns = "";
+        $scope.tipoIns = "";
+        $scope.vefIns = "";
+        modalnovisible(numero);
+    }
+    function modalvisible(x) {
+        document.getElementById("demo-modal"+x+"").style.visibility = "visible";
+        document.getElementById("demo-modal"+x+"").style.opacity = 1;
+        disable_scroll();
+        disable_scroll_mobile();
+    }
+    function modalnovisible(x) {
+        document.getElementById("demo-modal"+x+"").style.visibility = "hidden";
+        document.getElementById("demo-modal"+x+"").style.opacity = 0;
+        enable_scroll();
+        enable_scroll_mobile();
+    }
+     
+
+
+
+
+
+
+
+//////////// - Modaeles no scroll - ////////////
+  // PREVENT DEFAULT HANDLER
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
+  }
+  // PREVENT SCROLL KEYS
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  // left: 37, up: 38, right: 39, down: 40,
+  // (Source: http://stackoverflow.com/a/4770179)
+  function keydown(e) {
+    var keys = [32,33,34,35,36,37,38,39,40];
+    for (var i = keys.length; i--;) {
+      if (e.keyCode === keys[i]) {
+        preventDefault(e);
+        return;
+      }
+    }
+  }
+  // PREVENT MOUSE WHEEL
+  function wheel(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+  }
+  // DISABLE SCROLL
+  function disable_scroll() {
+    if (document.addEventListener) {
+      document.addEventListener('wheel', wheel, false);
+      document.addEventListener('mousewheel', wheel, false);
+      document.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    else {
+      document.attachEvent('onmousewheel', wheel);
+    }
+    document.onmousewheel = document.onmousewheel = wheel;
+    document.onkeydown = keydown;
+    
+    x = window.pageXOffset || document.documentElement.scrollLeft,
+    y = window.pageYOffset || document.documentElement.scrollTop,
+    window.onscroll = function() {
+      window.scrollTo(x, y);
+    };
+    // document.body.style.overflow = 'hidden'; // CSS
+    disable_scroll_mobile();
+  }
+  // ENABLE SCROLL
+  function enable_scroll() {
+    if (document.removeEventListener) {
+      document.removeEventListener('wheel', wheel, false);
+      document.removeEventListener('mousewheel', wheel, false);
+      document.removeEventListener('DOMMouseScroll', wheel, false);
+    }
+    document.onmousewheel = document.onmousewheel = document.onkeydown = null;
+    window.onscroll = function() {};
+    // document.body.style.overflow = 'auto'; // CSS
+    enable_scroll_mobile();
+  }
+  
+  // MOBILE
+  function disable_scroll_mobile(){
+    document.addEventListener('touchmove', preventDefault, false);
+  }
+  function enable_scroll_mobile(){
+    document.removeEventListener('touchmove', preventDefault, false);
+  }
 })
