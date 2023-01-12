81e89a7458c5ef17499ec94f6f787515bc2c4d44
mrajado <m_rajado@fpzornotza.com>
2023-01-12 13:20:26 +0100

    error undefined en transferencia

--- view/js/transferencia.js
+++ view/js/transferencia.js
@@ -117,11 +117,6 @@ MyApp.controller('miController', function ($scope, $http) {
     }
 
     $scope.cerrarCuentas = function (numero) {
-        $scope.dniIns = "";
-        $scope.nombreIns = "";
-        $scope.contrsenaIns = "";
-        $scope.tipoIns = "";
-        $scope.vefIns = "";
         modalnovisible(numero);
     }
     function modalvisible(x) {
@@ -133,77 +128,6 @@ MyApp.controller('miController', function ($scope, $http) {
         document.getElementById("demo-modal" + x + "").style.visibility = "hidden";
         document.getElementById("demo-modal" + x + "").style.opacity = 0;
 
-    }
-
-
-    //////////// - Modaeles no scroll - ////////////
-    // PREVENT DEFAULT HANDLER
-    function preventDefault(e) {
-        e = e || window.event;
-        if (e.preventDefault) {
-            e.preventDefault();
-        }
-        e.returnValue = false;
-    }
-    // PREVENT SCROLL KEYS
-    // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-    // left: 37, up: 38, right: 39, down: 40,
-    // (Source: http://stackoverflow.com/a/4770179)
-    function keydown(e) {
-        var keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
-        for (var i = keys.length; i--;) {
-            if (e.keyCode === keys[i]) {
-                preventDefault(e);
-                return;
-            }
-        }
-    }
-    // PREVENT MOUSE WHEEL
-    function wheel(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-    }
-    // DISABLE SCROLL
-    function disable_scroll() {
-        if (document.addEventListener) {
-            document.addEventListener('wheel', wheel, false);
-            document.addEventListener('mousewheel', wheel, false);
-            document.addEventListener('DOMMouseScroll', wheel, false);
-        }
-        else {
-            document.attachEvent('onmousewheel', wheel);
-        }
-        document.onmousewheel = document.onmousewheel = wheel;
-        document.onkeydown = keydown;
-
-        x = window.pageXOffset || document.documentElement.scrollLeft,
-            y = window.pageYOffset || document.documentElement.scrollTop,
-            window.onscroll = function () {
-                window.scrollTo(x, y);
-            };
-        // document.body.style.overflow = 'hidden'; // CSS
-        disable_scroll_mobile();
-    }
-    // ENABLE SCROLL
-    function enable_scroll() {
-        if (document.removeEventListener) {
-            document.removeEventListener('wheel', wheel, false);
-            document.removeEventListener('mousewheel', wheel, false);
-            document.removeEventListener('DOMMouseScroll', wheel, false);
-        }
-        document.onmousewheel = document.onmousewheel = document.onkeydown = null;
-        window.onscroll = function () { };
-        // document.body.style.overflow = 'auto'; // CSS
-        enable_scroll_mobile();
-    }
-
-    // MOBILE
-    function disable_scroll_mobile() {
-        document.addEventListener('touchmove', preventDefault, false);
-    }
-    function enable_scroll_mobile() {
-        document.removeEventListener('touchmove', preventDefault, false);
     }
 
 
@@ -238,6 +162,7 @@ MyApp.controller('miController', function ($scope, $http) {
             alert(response.data.error);
 
             vercuentas();
+            //vercuentasNoPersonales();
 
             if (response.data.error == "Completado"){
                 $scope.SeleccionT = false;
