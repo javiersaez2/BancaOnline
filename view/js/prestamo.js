03d55c5a5d26142a7234ddabf8f294e88eda8713
javiersaez2 <asaez22.az@gmail.com>
2023-01-13 09:12:38 +0100

    Mostrar cuenta en todos los admin

--- view/js/prestamo.js
+++ view/js/prestamo.js
@@ -1,6 +1,50 @@
 var MyApp = angular.module('MyApp', []);
 MyApp.controller('miController', function ($scope, $http) {
 
+
+
+
+
+
+
+    //verificar usuario
+    $scope.loggedVerify = function () {
+        $http({
+            url: "../../controller/cLoggedVerify.php",
+            method: "POST"
+        }).then(function (response) {
+            if (response.data.error != "logged") {
+                $scope.cuentaUsuario = false;
+                $scope.botonAdmin = false;
+                $scope.butonLogin = true;
+            } else {
+                $scope.butonLogOut = true;
+                $scope.butonLogin = false;
+
+                if (response.data.tipo == 1) {
+                    $scope.botonAdmin = true;
+                    $scope.cuentaUsuario = true;
+                } else {
+                    $scope.cuentaUsuario = true;
+                    $scope.botonAdmin = false;
+                }
+            }
+        }).catch(function (response) {
+            console.error("Ocurrio un error", response.status, response.data);
+        })
+    }
+    $scope.logout = function () {
+        $http({
+            url: "/controller/cLogout.php",
+            method: "POST"
+        }).then(function () {
+            window.location.href = "/index.html";
+            $scope.butonLogOut = false;
+        }).catch(function () {
+            console.error("Ocurrio un error", response.status, response.data);
+        })
+    }
+
     let Amortizazioa = 0.00;
     let Aldiak = 0.00;
     let Kuota = 0.00;
@@ -231,45 +275,4 @@ MyApp.controller('miController', function ($scope, $http) {
 
 
 
-
-
-
-
-    //verificar usuario
-    $scope.loggedVerify = function () {
-        $http({
-            url: "../../controller/cLoggedVerify.php",
-            method: "POST"
-        }).then(function (response) {
-            if (response.data.error != "logged") {
-                $scope.cuentaUsuario = false;
-                $scope.botonAdmin = false;
-                $scope.butonLogin = true;
-            } else {
-                $scope.butonLogOut = true;
-                $scope.butonLogin = false;
-
-                if (response.data.tipo == 1) {
-                    $scope.botonAdmin = true;
-                    $scope.cuentaUsuario = false;
-                } else {
-                    $scope.cuentaUsuario = true;
-                    $scope.botonAdmin = false;
-                }
-            }
-        }).catch(function (response) {
-            console.error("Ocurrio un error", response.status, response.data);
-        })
-    }
-    $scope.logout = function () {
-        $http({
-            url: "/controller/cLogout.php",
-            method: "POST"
-        }).then(function () {
-            window.location.href = "/index.html";
-            $scope.butonLogOut = false;
-        }).catch(function () {
-            console.error("Ocurrio un error", response.status, response.data);
-        })
-    }
 })
